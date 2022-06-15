a8ed29f63034fb49c401ce01475a2fd79a6145db
finani <10ezenfinani@gmail.com>
2022-06-16 01:14:58 +0900

    2.11 Returns

--- app.js
+++ app.js
@@ -111,3 +111,48 @@ divide(98, 20);
 console.log(player.name);
 player.sayHello("lynn");
 player.sayHello("nico");
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  power: function (a, b) {
+    // return Math.pow(a, b);
+    return a ** b;
+  }
+}
+
+console.log(calculator.plus(2,3));
+console.log(calculator.minus(2,3));
+console.log(calculator.times(2,3));
+console.log(calculator.divide(2,3));
+console.log(calculator.power(2,3));
+
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(powerResult);
+
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
+}
+
+const krAge = calculateKrAge(age);
+console.log(krAge);
