06712c4b6a06fa18fad7016318aa8bb132d29611
finani <10ezenfinani@gmail.com>
2022-06-19 19:47:19 +0900

    3.2 Searching For Elements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.title = "Hello! From JS!";
 
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
 console.log(title);
 console.dir(title);
 
@@ -8,3 +9,16 @@ title.innerText = "Got you!";
 
 console.log(title.id);
 console.log(title.className);
+
+// const hellos = document.getElementsByClassName("hello");
+const hellos = document.querySelectorAll(".hello");
+console.log(hellos);
+
+const h1s = document.getElementsByTagName("h1");
+console.log(h1s);
+
+const h3s = document.querySelector(".hello h3");
+console.log(h3s);
+
+const h3sAll = document.querySelectorAll(".hello h3");
+console.log(h3sAll);
