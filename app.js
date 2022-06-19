e67ccaa770b44f51c891c8c333dac7ab0dca1dbc
finani <10ezenfinani@gmail.com>
2022-06-19 23:20:02 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const username = loginInput.value;
-  localStorage.setItem("username", username);
+function paintGreetings(username) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  // greeting.innerHTML = "Hello " + username;
   greeting.innerHTML = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  console.log(username);
-});
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+  });
+} else {
+  paintGreetings(savedUsername);
+}
 
 link.addEventListener("click", (event) => {
   event.preventDefault();
