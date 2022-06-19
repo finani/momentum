e37cbb73cb8c44a2c8d79e345341d4df279a3a35
finani <10ezenfinani@gmail.com>
2022-06-19 22:49:00 +0900

    4.3 Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector('#login-form input');
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const username = loginInput.value;
   console.log(username);
   console.log(event);
+});
+
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.dir(event);
 });
