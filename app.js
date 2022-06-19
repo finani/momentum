00773f2227cc412be6c513d0e43f51d76847a37a
finani <10ezenfinani@gmail.com>
2022-06-19 22:59:50 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  // greeting.innerHTML = "Hello " + username;
+  greeting.innerHTML = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
   console.log(username);
-  console.log(event);
 });
 
 link.addEventListener("click", (event) => {
   event.preventDefault();
-  console.dir(event);
 });
