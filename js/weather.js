fc16ed9c470016318030329bf492065d3975f7d3
finani <10ezenfinani@gmail.com>
2022-06-23 01:04:00 +0900

    8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,18 @@
+const API_KEY = "498c1a7696b40b463ee16dfe9bb99895";
+
 const onGeoOk = (position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log("You live in", lat, lon);
-};
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      const weather = document.querySelector("#weather span:first-child")
+      const city = document.querySelector("#weather span:last-child")
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    })
+  });
+}
 
 const onGeoError = () => {
   alert("Can't find you. No weather for you.");
